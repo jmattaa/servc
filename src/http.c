1b2a43aea8ab2492327339ed9786f81ea2376056
jmattaa <mattajonathan1@gmail.com>
2025-05-13 15:51:57 +0200

    yeah, tryna serve dirs but works like shi

--- src/http.c
+++ src/http.c
@@ -1,6 +1,8 @@
+// TODO: many 404 messages should be 500!!!!!! but yo no orka
 #include "http.h"
 #include "ftype.h"
 #include "logger.h"
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -9,7 +11,11 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-static char *http_get(char *targ, size_t *res_len);
+#define HEADER_SIZE 512
+
+static char *http_get(const char *targ, size_t *res_len);
+static char *http_getfile(struct stat st, const char *targ, size_t *res_len);
+static char *http_getdir(struct stat st, const char *targ, size_t *res_len);
 
 servc_http *servc_http_parse(char *req)
 {
@@ -103,7 +109,7 @@ static const char *notfound =
                      "Content-Length:" SERVC_HTTP_404_MSGLEN "\r\n"
                      "\r\n" SERVC_HTTP_404_MSG;
 
-static char *http_get(char *targ, size_t *res_len)
+static char *http_get(const char *targ, size_t *res_len)
 {
     if (targ == NULL)
     {
@@ -118,6 +124,20 @@ static char *http_get(char *targ, size_t *res_len)
     if (targ[0] == '/')
         targ++; // skip '/'
 
+    struct stat st;
+    if (stat(targ, &st) == -1)
+    {
+        servc_logger_error("Failed to stat file: %s\n", targ);
+        *res_len = strlen(notfound);
+        return strdup(notfound);
+    }
+    if (S_ISDIR(st.st_mode))
+        return http_getdir(st, targ, res_len);
+    return http_getfile(st, targ, res_len);
+}
+
+static char *http_getfile(struct stat st, const char *targ, size_t *res_len)
+{
     int fd = open(targ, O_RDONLY);
     if (fd == -1)
     {
@@ -126,15 +146,7 @@ static char *http_get(char *targ, size_t *res_len)
         return strdup(notfound);
     }
 
-    struct stat st;
-    if (fstat(fd, &st) == -1)
-    {
-        servc_logger_error("Failed to stat file: %s\n", targ);
-        close(fd);
-        return NULL;
-    }
-
-#define HEADER_SIZE 512
+    // todo we repeat this header string again in getdir FIX IT!!!!!
     char header[HEADER_SIZE];
     const char *mime = servc_mime(targ);
     int header_len = snprintf(header, sizeof(header),
@@ -172,6 +184,97 @@ static char *http_get(char *targ, size_t *res_len)
     }
 
     close(fd);
+    *res_len = total_len;
+    return res;
+}
+
+static char *http_getdir(struct stat st, const char *targ, size_t *res_len)
+{
+    // kinda like https://github.com/jmattaa/laser
+    // the function called laser_process_entries in src/laser.c
+
+    DIR *dir = opendir(targ);
+    if (dir == NULL)
+    {
+        servc_logger_error("Failed to open directory: %s\n", targ);
+        *res_len = strlen(notfound);
+        return strdup(notfound);
+    }
+
+    struct dirent *ent = malloc(sizeof(struct dirent));
+    if (!ent)
+    {
+        servc_logger_error("Memory allocation failed\n");
+        closedir(dir);
+        *res_len = strlen(notfound);
+        return strdup(notfound);
+    }
+
+    char *res = malloc(1);
+    if (!res)
+    {
+        servc_logger_error("Memory allocation failed\n");
+        free(ent);
+        closedir(dir);
+        *res_len = strlen(notfound);
+        return strdup(notfound);
+    }
+    char *enttemp = "<li><a href=\"%s\">%s</a></li>\r\n";
+    while ((ent = readdir(dir)) != NULL)
+    {
+        if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+            continue;
+
+        res = realloc(res, strlen(res) + strlen(enttemp) +
+                               (strlen(ent->d_name) * 2) + 1);
+        if (!res)
+        {
+            servc_logger_error("Memory allocation failed\n");
+            free(ent);
+            closedir(dir);
+            *res_len = strlen(notfound);
+            return strdup(notfound);
+        }
+
+        snprintf(res + strlen(res), strlen(enttemp) + strlen(ent->d_name) + 1,
+                 enttemp, ent->d_name, ent->d_name);
+    }
+
+    free(ent);
+    closedir(dir);
+
+    if (res == NULL)
+    {
+        servc_logger_error("Failed to read directory: %s\n", targ);
+        *res_len = strlen(notfound);
+        return strdup(notfound);
+    }
+
+    char header[HEADER_SIZE];
+    const char *mime = servc_mime(targ);
+    int header_len = snprintf(header, sizeof(header),
+                              SERVC_HTTP_PROTO "200 OK\r\n"
+                                               "Server: servc\r\n"
+                                               "Content-Type: %s\r\n"
+                                               "Content-Length: %zu\r\n"
+                                               "\r\n",
+                              mime, (size_t)st.st_size);
+
+    if (header_len < 0 || header_len >= HEADER_SIZE)
+    {
+        servc_logger_error("Header generation failed or truncated\n");
+        free(res);
+        return NULL;
+    }
+
+    size_t total_len = header_len + strlen(res);
+    res = realloc(res, total_len);
+    if (!res)
+    {
+        servc_logger_error("Memory allocation failed\n");
+        return NULL;
+    }
+    memcpy(res, header, header_len);
     *res_len = total_len;
     return res;
 }
